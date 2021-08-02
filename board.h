fd68955d81fc30c60cdb095d545fa3e399c69aec
supsun <supsun-sockol@yandex.ru>
2021-08-02 13:36:22 +0300

    add object

--- board.h
+++ board.h
@@ -1,20 +1,26 @@
+#ifndef BOARD_H
+#define BOARD_H
 include <list>
-class hexagon {
-    *hexagon left;
-    *hexagon right;
-    *hexagon top_left;
-    *hexagon top_right;
-    *hexagon bot_left;
-    *hexagon bot_right;
+class hexagon
+{
+    public:
+        *hexagon left;
+        *hexagon right;
+        *hexagon top_left;
+        *hexagon top_right;
+        *hexagon bot_left;
+        *hexagon bot_right;
 
-    int lock_status;
-    list <
-    list <object>  hex_objects;
+        int lock_status;
+        list <
+        list <object>  hex_objects;
 }
-class board {
-    board(int radius);
-    int radius;
-    *hexagon center;
-    hexagon GetHexagon(int x, int y, int z);
-
+class board
+{
+    public:
+        board(int radius);
+        int radius;
+        *hexagon center;
+        hexagon GetHexagon(int x, int y, int z);
 }
+#endif // BOARD_H
---
+++ object.h
@@ -0,0 +1,16 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+
+class object
+{
+    public:
+        object();
+        virtual ~object();
+
+    protected:
+
+    private:
+};
+
+#endif // OBJECT_H
