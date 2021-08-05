6669d6a9e4e2e850103744cf72463919c99c0d1f
supsun <supsun-sockol@yandex.ru>
2021-08-05 15:06:37 +0300

    finish pseudographic interface

--- main.cpp
+++ main.cpp
@@ -2,17 +2,23 @@
 #include <map>
 #include "object.h"
 #include "board.h"
+#include "klibriry.h"
 int main()
 {
     const int radius = 3;
     board B(radius);
-    char n;
-    std::map <char, int> direct = {{0, 6}, {'a', 0}, {'w', 1}, {'e', 2}, {'d', 3}, {'x', 4}, {'z', 5}};
+    char d;
+    std::map <char, int> direct = {{'a', 0}, {'w', 1}, {'e', 2}, {'d', 3}, {'x', 4}, {'z', 5}, {'s', 6}};
+    std::map <char, int> :: iterator it;
     hero H(B.center);
     while(1){
         B.print();
         std::cout<<std::endl;
-        std::cin>>n;
-        H.move(direct[n]);
+        std::cin>>d;
+        it = direct.find(d);
+        if (it != direct.end()){
+            H.move(it->second);
+        }
+
     }
 }
