30a1c3db03957acd29a9e46e61f4d91354f1fac4
supsun <supsun-sockol@yandex.ru>
2021-08-14 13:04:30 +0300

    start to add Event Queue

--- board.h
+++ board.h
@@ -2,6 +2,7 @@
 #define BOARD_H
 #include "object.h"
 #include <list>
+#include "EventQueue.h"
 class board;
 
 class hexagon
@@ -33,6 +34,7 @@ class board
         int radius;
         hexagon* center;
         std::list <object*>  all_objects;
+        EventQueue event_queue;
 
         void print();
         hexagon* HexByCoordinates(int x, int y);
