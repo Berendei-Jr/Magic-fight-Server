2072392c28e937956dc8602a4e0c006351ec074c
Supsun <supsun-sockol@yandex.ru>
2021-12-29 00:59:34 +0300

    change uml

---
+++ _build/_build/CMakeFiles/server_app.dir/cmake_clean.cmake
@@ -0,0 +1,23 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/server_app.dir/include/NetCommon.cpp.o"
+  "CMakeFiles/server_app.dir/include/NetCommon.cpp.o.d"
+  "CMakeFiles/server_app.dir/sources/EventQueue.cpp.o"
+  "CMakeFiles/server_app.dir/sources/EventQueue.cpp.o.d"
+  "CMakeFiles/server_app.dir/sources/action_classes.cpp.o"
+  "CMakeFiles/server_app.dir/sources/action_classes.cpp.o.d"
+  "CMakeFiles/server_app.dir/sources/actions.cpp.o"
+  "CMakeFiles/server_app.dir/sources/actions.cpp.o.d"
+  "CMakeFiles/server_app.dir/sources/board.cpp.o"
+  "CMakeFiles/server_app.dir/sources/board.cpp.o.d"
+  "CMakeFiles/server_app.dir/sources/object.cpp.o"
+  "CMakeFiles/server_app.dir/sources/object.cpp.o.d"
+  "CMakeFiles/server_app.dir/sources/projectiles.cpp.o"
+  "CMakeFiles/server_app.dir/sources/projectiles.cpp.o.d"
+  "libserver_app.a"
+  "libserver_app.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server_app.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
